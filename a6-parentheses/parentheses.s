65d19ce1a8b1a6bd923a4fcead8a628e6793310f
tobizal <tobizal@github.com>
2023-10-14 23:50:22 +0200

    String comparison works.

--- a6-parentheses/parentheses.s
+++ a6-parentheses/parentheses.s
@@ -1,4 +1,25 @@
+# ASCII coes for parentheses
+# (	40
+# )	41
+# <	60
+# >	62
+# [	91
+# ]	93
+# {	123
+# }	125
+
 .data
+
+open_brackets:
+	.ascii	"("
+	.ascii	"<"
+	.ascii	"["
+	.ascii	"{"
+closed_brackets:
+	.ascii	")"
+	.ascii	">"
+	.ascii	"]"
+	.ascii	"}"
 
 input:		.asciz	"[]{}[()()]"
 
@@ -11,18 +32,29 @@ main:
 	pushq	%rbp			# store the caller's base pointer on the stack
 	movq	%rsp, %rbp		# update the base pointer for this stack frame
 
-	movq	$input, %rdx		# move the string pointer to %RDX
+	movq	$input, %rdi		# move the string pointer to %RDX
+	leaq	open_brackets, %rsi	# move the addres of first bracket to check for
 
-	movzb	(%rdx), %eax 		# zero extend the character (byte) to 32-bit eax
-	pushw	(%eax)			# so that it can be pushed onto the stack
-	incq	%rdx			# increment rdx to point to the next character
+	movq	$3, %rcx		# rcx as a loop counter for LOOP
+open_check:
+	cmpsb				# compare ASCII characters at RSI and RDi loc
+	pushf				# preserve the flags reg after cmp
+	dec	%rdi			# compensate for automatic inc after cmpsb
+	popf				# pop back rflags
+	je	is_open_bracket		# if ZF=1 its an open bracket, jump to corresp code
+	loop	open_check
 
-loop:
-	movzb	(%rdx), %eax 		# zero extend the next character (byte) to 32-bit eax
-	popq	%rcx			# pop the character from the top of the stack
-
+	movq	$3, %rcx
+closed_check:
+	cmpsb
+	pushf
+	dec	%rdi
+	popf
+	je	is_closed_bracket
+	loop	closed_check
 	
-	
+is_open_bracket:
+is_clsoed_bracket:
 
 	# epilogue
 	movq	%rbp, %rsp		# clear the stack variables
