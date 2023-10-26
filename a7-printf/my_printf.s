51df6a1df7301b2a3718ec9fa45f52dda195030f
tobizal <tobizal@github.com>
2023-10-26 17:37:49 +0200

    Generally works. Only stack arguments dont work.

--- a7-printf/my_printf.s
+++ a7-printf/my_printf.s
@@ -4,9 +4,19 @@ uint_str:	.quad 0x0
 int_str:	.quad 0x0
 		.quad 0x0
 
-output:		.asciz	"Hello World!"
-.text
+output:		.asciz	"Hello World!\n"
+output1:	.asciz	"My name is %s. I am %u years old. My favourite number is %d.\n"
+output2:	.asciz 	"%s%s%s%s%s%s%s%s."
 
+name:		.asciz	"Tobiasz"
+
+argument_1:	.quad 0x0
+argument_2:	.quad 0x0
+argument_3:	.quad 0x0
+argument_4:	.quad 0x0
+argument_5:	.quad 0x0
+
+.text
 .global main
 .global my_printf
 
@@ -17,23 +27,124 @@ output:		.asciz	"Hello World!"
 # @param RDX - argument 2
 # @param RCX - argument 3
 # @param R8 - argument 4
-# @param
+# @param R9 - argument 5
+# @param STACK - next arguments
 my_printf:
 	# prologue
 	pushq	%rbp			# preserve the caller's base pointer
 	movq	%rsp, %rbp		# update the base pointer for this stack frame
+	
+	# load the arguments 
+	movq	%rsi, argument_1
+	movq	%rdx, argument_2
+	movq	%rcx, argument_3
+	movq	%r8, argument_4
+	movq	%r9, argument_5
 
 	# code goes here ...	
 	# @variable %RCX - counter of the number of the variable
 	movq	$0, %rcx
+	loop:
+	movb	(%rdi), %al		# character frin the string in RAX
+	cmpb	$0, %al 	# check if character is 0
+	je	my_printf_end	# if zero, its the end of the string so end
+		
+	cmpb	$'%', %al	# check if its a %
+	je	percent_sign_found	#jump to corresponding code
 	
+	push	%rdi
+	pushq	%rcx
+	call	print_char	# just print the character if none of the above
+	popq	%rcx
+	pop	%rdi
+	jmp	loop_next	
 	
+	percent_sign_found:
+	incq	%rdi		# increment the character pointer
+	cmpb	$'%', (%rdi)	# check if the next character is % as well
+	jne	arg_found	# if not a percentage sign, print the argument
+	
+	#else
+	pushq	%rdi
+	pushq	%rcx
+	call	print_char	# otherwise, just print the character
+	popq	%rcx
+	popq	%rdi
+	jmp	loop_next
 
+	arg_found:
+	call	load_arg	# the argument is in %rax now
+	cmpb	$'u', (%rdi)	# check arg type
+	je	uint_arg	
+
+	cmpb	$'d', (%rdi)
+	je	int_arg
+
+	cmpb	$'s', (%rdi)
+	je	str_arg	
+
+	
+	uint_arg:
+	pushq	%rdi
+	pushq	%rcx
+	movq	%rax, %rdi
+	call	uint_to_str
+	movq	$uint_str, %rdi
+	call	print_str
+	popq	%rcx
+	popq	%rdi
+	jmp	loop_next
+	int_arg:
+	pushq	%rdi
+	pushq	%rcx
+	movq	%rax, %rdi
+	call	int_to_str
+	movq	$int_str, %rdi
+	call	print_str
+	popq	%rcx
+	popq	%rdi
+	jmp	loop_next
+	str_arg:
+	pushq	%rdi
+	pushq	%rcx
+	movq	%rax, %rdi
+	call	print_str
+	popq	%rcx
+	popq	%rdi
+	
+	loop_next:
+	incq	%rdi		# increment the character pointer
+	jmp	loop		# proceed to the next iteration
+			
+my_printf_end:
 	# epilogue
 	movq	%rbp, %rsp		# clear the stack varialbes
 	popq	%rbp			# restore the caller's base pointer
 	ret
 
+
+#load_arg 
+# @return %RAX - the value of the current argument taken from the register parameters
+load_arg:
+	cmpq	$4, %rcx	# check the rcx arg counter
+	jle	arg_vars	# get the arguments from argument variables (registers)
+
+	#else, get the from the stack	
+	movq	%rcx, %rax	# copy the counter value to rax
+	subq	$5, %rax	# correct rax to properly index the stack variable
+	neg	%rax
+	subq	$2, %rax
+	movq	(%rbp, %rax, 8), %rax
+	jmp	load_arg_end
+	
+	arg_vars:
+	movq	%rcx, %rax	# copy the counter value to rax
+	movq	$argument_1, %rax
+	movq	(%rax, %rcx, 8), %rax
+	
+	load_arg_end:
+	incq	%rcx		# increment the arg counter
+	ret
 # print_char subroutine
 # @param RDI - address of the character
 print_char:
@@ -217,7 +328,29 @@ main:
 	# test printchar
 	movq	$output, %rdi
 	call 	print_char
+	
+	# test print_str
+	movq	$name, %rdi
+	call 	print_str
 
+	# test printf
+	movq	$output1, %rdi
+	movq	$name, %rsi
+	movq	$20, %rdx
+	movq	$-42, %rcx
+	call	my_printf
+
+	# test printf 
+	movq	$output2, %rdi
+	movq	$name, %rsi
+	movq	$name, %rdx
+	movq	$name, %rcx
+	movq	$name, %r8
+	movq	$name, %r9
+	pushq	$name
+	pushq	$name
+	pushq	$name
+	call	my_printf
 	# epilogue
 	movq	%rbp, %rsp		# clear the stack varialbes
 	popq	%rbp			# restore the caller's base pointer
